c33775b42fc901731560caf6ca4621b573ca2252
Akshay Mathur <akshay.mathur@sjsu.edu>
2016-04-14 12:41:28 -0700

    Added code to recommend movie names instead of movie ids

--- src/main/java/com/simple_recommender_system/ItemBasedRecommendationSystem.java
+++ src/main/java/com/simple_recommender_system/ItemBasedRecommendationSystem.java
@@ -2,6 +2,7 @@ package com.simple_recommender_system;
 
 import java.io.*;
 import java.util.*;
+
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.file.*;
 import org.apache.mahout.cf.taste.impl.neighborhood.*;
@@ -22,6 +23,22 @@ public class ItemBasedRecommendationSystem {
 
 			DataModel model = new FileDataModel(new File("resources/data_sets/PreprocessedDataForMahout.csv"));
 			
+			/*Akshay
+			 * Parsing Movied Id vs Movie Name file and creating a Hash Map so the we can output
+			 * movie names instead of the movie Ids
+			 * */
+			String csvFile = "resources/data_sets/movieid-movie.csv";
+			BufferedReader br = null;
+			String line = "";
+			String cvsSplitBy = ",";
+			HashMap<Integer, String> movieMap= new HashMap<Integer, String>();
+			br = new BufferedReader(new FileReader(csvFile));
+			while ((line = br.readLine()) != null) {
+				String[] movieData = line.split(cvsSplitBy);
+				//System.out.println(movieData[0]+"---"+movieData[1]);
+				movieMap.put(Integer.parseInt(movieData[0]), movieData[1]);
+				
+			}
 			//Step 2:- Create ItemSimilarity Matrix
 			ItemSimilarity similarity = new PearsonCorrelationSimilarity(model);
 			
@@ -30,18 +47,21 @@ public class ItemBasedRecommendationSystem {
 			GenericItemBasedRecommender recommender = new GenericItemBasedRecommender(model,similarity);
 			
 			//Step 4:- Call the Generated Recommender in previous step to getting 
-			//recommendation for particular user or Item	
-			List<RecommendedItem> recommendations = recommender.recommend(792, 3);
-			
+			//recommendation for particular user or Item
+			int userID=292;
+			List<RecommendedItem> recommendations = recommender.recommend(userID, 3);
+			System.out.println("Movies Being recommended for User:"+userID);
 			for (RecommendedItem recommendation : recommendations) {
-			  System.out.println(recommendation);
+				//System.out.println((int)recommendation.getItemID());
+				System.out.println(movieMap.get((int)recommendation.getItemID()));
 			}
-
+			/* Commenting this as it was for our own understanding
 			List<RecommendedItem> similarItems = recommender.mostSimilarItems(1, 3);
 			
 			for (RecommendedItem recommendation : similarItems) {
 				  System.out.println(recommendation);
 			}
+			*/
 			
 		}
 		catch (Exception e) {
--- src/main/java/com/simple_recommender_system/UserBasedRecommendationSystem.java
+++ src/main/java/com/simple_recommender_system/UserBasedRecommendationSystem.java
@@ -1,6 +1,9 @@
 package com.simple_recommender_system;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
@@ -23,7 +26,7 @@ public class UserBasedRecommendationSystem {
 			/* Converting the CSV File data present in the form of (User, Item, Rating) into Data Model */
 			DataModel dm = new FileDataModel(new File("resources/data_sets/PreprocessedMCDataForMahout.csv"));
 			
-			System.out.println("DataModel: " + dm);
+			//System.out.println("DataModel: " + dm);
 			
 			/* In this recommendation system we will be using Pearson Correlation Method for calculating the Similarity */
 			UserSimilarity similarity = new PearsonCorrelationSimilarity(dm);
@@ -34,18 +37,39 @@ public class UserBasedRecommendationSystem {
 			UserBasedRecommender recommender = new GenericUserBasedRecommender(dm, neighborhood, similarity);
 
 			/* Here I am trying to recommend items for the user with User Id to 2 and number of items as 3*/
-			List<RecommendedItem> recommendations = recommender.recommend(13, 5);
+			int userID = 13;
+			List<RecommendedItem> recommendations = recommender.recommend(userID, 5);
 			
 			long[] similarUserIds = recommender.mostSimilarUserIDs(13, 5);
 			
-			/* Display all the items recommended to the user with User Id 2 */
-			for (RecommendedItem recommendation : recommendations) {
-			  System.out.println(recommendation);
+			/*Akshay
+			 * Parsing Movied Id vs Movie Name file and creating a Hash Map so the we can output
+			 * movie names instead of the movie Ids
+			 * */
+			
+			String csvFile = "resources/data_sets/movieid-movie.csv";
+			BufferedReader br = null;
+			String line = "";
+			String cvsSplitBy = ",";
+			HashMap<Integer, String> movieMap= new HashMap<Integer, String>();
+			br = new BufferedReader(new FileReader(csvFile));
+			while ((line = br.readLine()) != null) {
+				String[] movieData = line.split(cvsSplitBy);
+				//System.out.println(movieData[0]+"---"+movieData[1]);
+				movieMap.put(Integer.parseInt(movieData[0]), movieData[1]);
+				
 			}
 			
+			/* Display all the items recommended to the user with User Id 2 */
+			System.out.println("Recommending movies for UserId:"+ userID);
+			for (RecommendedItem recommendation : recommendations) {
+				System.out.println(movieMap.get((int)recommendation.getItemID()));
+			}
+			/* Commenting this as it was for our own understanding
 			for(long userId : similarUserIds)	{
 				System.out.println(userId);
 			}
+			*/
 			
 		}catch (Exception e) {
 			System.out.println("There was an error.");
